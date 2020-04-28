5bd07cdd338c26f1778ba5862ca97effe6e122e3
Eric Crescioni <ecam900@gmail.com>
2020-04-28 12:24:57 -0400

    Add snackbar and authguard

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AngularFireAuth } from '@angular/fire/auth/auth';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { SnackService } from '../services/snack.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(
-    private afAuth: AngularFireAuth
-  ) {}
+  constructor(private afAuth: AngularFireAuth, private snack: SnackService) {}
 
   async canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-    ): Promise<boolean> {
-      const user = await this.afAuth.currentUser;
-      const isLoggedIn = !!user;
-
-      return isLoggedIn;
+  ): Promise<boolean> {
+    const user = await this.afAuth.currentUser;
+    const isLoggedIn = !!user;
+    if (!isLoggedIn) {
+      this.snack.authError();
+    }
+    return isLoggedIn;
   }
-  
 }
- 
