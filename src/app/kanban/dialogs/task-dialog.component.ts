867b9fac0a3b36123823a2040daeab5f54990689
Eric Crescioni <ecam900@gmail.com>
2020-04-30 17:04:05 -0400

    Add board creation dialog and start working on task creation dialog

---
+++ src/app/kanban/dialogs/task-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-dialog',
+  template: `
+    <p>
+      task-dialog works!
+    </p>
+  `,
+  styleUrls: ['./task-dialog.component.scss']
+})
+export class TaskDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
