7e78635facbd9c3d0cc9a2aa95260c63247e0198
Eric Crescioni <ecam900@gmail.com>
2020-05-04 16:29:23 -0400

    Add tasks dialog view, typescript and styles. Add it to the board view.

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { BoardService } from './../board.service';
+import { Task } from '../board.model';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent } from '../dialogs/task-dialog.component';
 
 @Component({
   selector: 'app-board',
@@ -10,10 +14,35 @@ import { BoardService } from './../board.service';
 export class BoardComponent {
   @Input() board;
 
-  constructor(private BoardService: BoardService) {}
+  constructor(private boardService: BoardService, private dialog: MatDialog) {}
 
   taskDrop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
-    this.BoardService.updateTasks(this.board.id, this.board.tasks);
+    this.boardService.updateTasks(this.board.id, this.board.tasks);
+  }
+
+  openDialog(task?: Task, idx?: number): void {
+    const newTask = { label: 'purple' };
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '500px',
+      data: task
+        ? { task: { ...task }, isNew: false, boardId: this.board.id, idx }
+        : { task: newTask, isNew: true },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        if (result.isNew) {
+          this.boardService.updateTasks(this.board.id, [
+            ...this.board.tasks,
+            result.task,
+          ]);
+        } else {
+          const update = this.board.tasks;
+          update.splice(result.idx, 1, result.task);
+          this.boardService.updateTasks(this.board.id, this.board.tasks);
+        }
+      }
+    });
   }
 }
